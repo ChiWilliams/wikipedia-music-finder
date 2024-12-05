e6c5b42da1b7674c0f761fa42696ef1d6f1e23f2
Kai Williams <154692413+ChiWilliams@users.noreply.github.com>
2024-12-05 15:03:50 -0500

    Cleaning up code

--- get_ulrs.py
+++ get_ulrs.py
@@ -1,64 +1,83 @@
 import requests, time
+from requests.exceptions import HTTPError
 from blingfire import text_to_sentences
+from typing import TypedDict, Dict
 
-def get_random_summaries(num_pages = 50):
-    url = 'https://en.wikipedia.org/w/api.php'
+WIKIPEDIA_ENDPOINT = 'https://en.wikipedia.org/w/api.php'
+
+def validate_wiki_response(response_json: dict) -> None:
+    if 'query' not in response_json:
+        raise ValueError("Missing 'query' key in response")
+    if 'pages' not in response_json['query']:
+        raise ValueError("Missing 'pages' in query")
+
+def get_random_ids(num_pages) -> list[str]:
     params = {
-    "action": "query",
-    "format": "json",
-    "list": "random",
-    "formatversion": "2",
-    "rnnamespace": "0",
-    "rnlimit": num_pages,
+        "action": "query",
+        "format": "json",
+        "list": "random",
+        "formatversion": "2",
+        "rnnamespace": "0",
+        "rnlimit": num_pages,
     }
 
-    response = requests.get(url, params=params)
+
+    response = requests.get(WIKIPEDIA_ENDPOINT, params=params, timeout=5)
     response.raise_for_status()
     data = response.json()
+    print(data)
 
-    page_ids = [str(x['id']) for x in data['query']['random']]
+    return [str(x['id']) for x in data['query']['random']]
 
-    summary_params = {
-    "action": "query",
-    "format": "json",
-    "prop": "extracts",
-    "exintro": True,
-    "explaintext": True,
-    "pageids": "|".join(page_ids)
+def build_getter_params(ids: list[str], continue_params: dict | None):
+    continue_params = continue_params or {}
+    params = {
+        "action": "query",
+        "format": "json",
+        "prop": "extracts",
+        "exintro": True,
+        "explaintext": True,
+        "pageids": "|".join(ids)
     }
+    params.update(continue_params)
+    return params
 
-    summaries = requests.get(url, params=summary_params)
-    summaries.raise_for_status()
-    summaries_JSON = summaries.json()
-    continue_params = summaries.json().get('continue',{})
-    while continue_params:
-        summary_params.update(continue_params)
-        summaries = requests.get(url, params = summary_params)
-        try:
-            summaries.raise_for_status()
-            new_response = summaries.json()
-            for page_id, page_data in new_response['query']['pages'].items():
+def get_wiki_pages(ids: list[str], continue_params: dict = None) -> dict:
+    params = build_getter_params(ids, continue_params)
+    response = requests.get(WIKIPEDIA_ENDPOINT, params=params)
+    response.raise_for_status()
+    return response.json()
+
+def merge_responses(base, new) -> None:
+    for page_id, page_data in new['query']['pages'].items():
                 if 'extract' in page_data:
-                    summaries_JSON['query']['pages'][page_id]['extract'] = page_data['extract']
-            print("---")
-            continue_params = new_response.get('continue', {})
-        except:
-            break
+                    base['query']['pages'][page_id]['extract'] = page_data['extract']
 
-    summaries_txt = [ x['extract'] for _,x in summaries_JSON['query']['pages'].items() if 'extract' in x]
+def get_full_summaries(ids: list[str]) -> list[str]:
+    # the API call can only take 50 or fewer IDS at a time
+    assert len(ids) <= 50
+
+    summaries = get_wiki_pages(ids, None)
+    continue_params = summaries.get('continue',{})
+    while continue_params:
+        new_summaries = get_wiki_pages(ids, continue_params)
+        merge_responses(summaries, new_summaries)
+        continue_params = new_summaries.get('continue', {})
+
+    summaries_txt = [ x['extract'] for _,x in summaries['query']['pages'].items() if 'extract' in x]
     return summaries_txt
 
-def first_sentences(summaries):
+def first_sentences(summaries: list[str]) -> list[str]:
     return [ text_to_sentences(summary).split('\n')[0] for summary in summaries ]
 
+def get_wikipedia_sentence_summaries(num_pages: int = 50) -> list[str]:
+    ids = get_random_ids(num_pages)
+    full_summaries = get_full_summaries(ids)
+    return first_sentences(full_summaries)
 
 if __name__ == "__main__":
-    start_time = time.time()
-    summs = get_random_summaries()
-    summs_got_time = time.time()
-    summaries = first_sentences(summs)
-    end_time = time.time()
-    print(summaries)
-    print(f"{len(summaries)=}")
-    print(f"This took {summs_got_time - start_time} seconds to get summaries")
-    print(f"This took {end_time-summs_got_time} seconds to split summaries")
+    ids = get_random_ids(500)
+
+    # summaries = get_wikipedia_sentence_summaries()
+    # print(summaries)
+    # print(f"{len(summaries)=}")
