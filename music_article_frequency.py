f193367a5ca89de5c5decc19a36a60d22d5fa6d5
Kai Williams <154692413+ChiWilliams@users.noreply.github.com>
2024-12-06 14:54:07 -0500

    Did some data analysis and plotting

--- music_article_frequency.py
+++ music_article_frequency.py
@@ -5,6 +5,36 @@
 # and distribution of length
 
 import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open("classifications.jsonl", 'r') as jsonl:
+    data = [json.loads(line) for line in jsonl]
+
+is_music = np.array([bool(x['is_music']) for x in data])
+
+# percent_music = sum(1 for x in data if x['is_music']) / len(data)
+percent_music = is_music.mean()
+print(percent_music) # 7.4% of the sample is music
+
+# now we calculate the distribution of gaps:
+music_indices = np.where(is_music)[0]
+gaps = np.diff(music_indices)
+print(music_indices)
+print(gaps)
+print(gaps.mean())
+
+
+# and now we graph!
+x = np.arange(1, max(gaps) + 1)
+plt.plot(x, 0.058 * (0.942)**(x-1), 'r--', label = 'Lower bound', linewidth = 2)
+plt.plot(x, 0.090 * (0.910)**(x-1), 'g--', label = 'Upper bound', linewidth = 2)
+plt.legend()
+plt.hist(gaps, bins=range(0, max(gaps) + 2, 5), density= 'true', alpha = 0.7)
+plt.xlabel('Gap Size')
+plt.ylabel('Frequency')
+plt.show()
+
 
 
 
