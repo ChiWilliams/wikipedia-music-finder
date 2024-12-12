8739d8ca7b99ad8804180931adc77ebce00f292c
Kai Williams <154692413+ChiWilliams@users.noreply.github.com>
2024-12-12 09:57:05 -0500

    refactor sample file into pytest fixture

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,7 +1,18 @@
 import pytest
 from wiki_music.utilities.data_processing import *
 
+@pytest.fixture
+def test_data_content_small():
+    return [
+    '{"summary": "Test 1", "is_music": true}\n',
+    '{"summary": "Test 2", "is_music": false}\n'
+    ]
 
+@pytest.fixture
+def test_jsonl_small(tmp_path, test_data_content_small):
+    test_file = tmp_path / "test_small.jsonl"
+    test_file.write_text("".join(test_data_content_small))
+    return test_file
 
 def test_get_custom_dataset():
     sample_data = get_custom_dataset()[:2]
@@ -10,16 +21,8 @@ def test_get_custom_dataset():
     assert len(sample_data) == 2
     assert all('summary' in item and 'is_music' in item for item in sample_data)
 
-def test_load_dataset(tmp_path):
-    #create temporary file
-    test_file = tmp_path / "test.jsonl"
-    test_file.write_text(
-    '{"summary": "Test 1", "is_music": true}\n'
-    '{"summary": "Test 2", "is_music": false}\n'
-    )
-
-    #Test loading
-    result = load_data(test_file)
+def test_load_dataset(test_jsonl_small):
+    result = load_data(test_jsonl_small)
     assert result[0]['summary'] == 'Test 1'
     assert len(result) == 2
     assert all(isinstance(item, dict) for item in result)
